4f15d8aaacb6660312e14317ae5f7eb3c1e6f103
Robert Wyszecki <robertwyszecki@gmail.com>
2022-01-20 09:06:34 +0100

    second section finished

--- app.py
+++ app.py
@@ -1,28 +1,92 @@
-from flask import Flask, request, make_response, jsonify
+from flask import Flask, jsonify, request
 
-# TODO check what __name__ return
-# print(f"{__name__=}")
+POSTS = [
+    {
+        "id": 1,
+        "title": "title 1",
+        "text": "text 1"
+    },
+    {
+        "id": 2,
+        "title": "title 2",
+        "text": "text 2"
+    },
+    {
+        "id": 3,
+        "title": "title 3",
+        "text": "text 3"
+    },
+    {
+        "id": 4,
+        "title": "title 4",
+        "text": "text 4"
+    }
+]
+
 
 app = Flask(__name__)
 
 
-@app.route("/")
-def index():
-    # print(request.headers)
-    print(request.method)
-    print(request.path)
-    print(request.url)
-    print(request.headers)
-    print(request.headers["Authorization"])
-    print(request.headers["Content-Type"])
-    print(request.json)
-    print(request.json["name"])
-    print(request.json.get("age"))
-    # response = make_response([{"id": 1, "title": "Title"}])
-    # # set response header type to json
-    # response.headers["Content-Type"] = "application/json"
-    # jsonify automatically converts response to json
-    response = jsonify([{"id": 1, "title": "Title"}])
+@app.route("/posts", methods=["GET", "POST"])
+def items():
+    response_data = {
+        "success": True,
+        "data": []
+    }
+
+    if request.method == "GET":
+        response_data["data"] = POSTS
+        return jsonify(response_data)
+    elif request.method == "POST":
+        data = request.json
+        POSTS.append(data)
+        if "id" not in data or "title" not in data or "text" not in data:
+            response_data["success"] = False
+            response_data["error"] = "Please provide all required information"
+            response = jsonify(response_data)
+            response.status = 400
+        else:
+            response_data["data"] = POSTS
+            response = jsonify(response_data)
+            response.status = 201
+        return response
+
+
+# wyciąganie wartości z rout
+@app.route("/posts/<int:post_id>")
+def item(post_id):
+    response_data = {
+        "success": True,
+        "data": []
+    }
+
+    # item = ""
+    # for post in POSTS:
+    #     if post["id"] == post_id:
+    #         item = post
+
+    try:
+        item = [post for post in POSTS if post["id"] == post_id][0]
+    except IndexError:
+        response_data["success"] = False
+        response_data["error"] = "Not found"
+        response = jsonify(response_data)
+        response.status_code = 404
+    else:
+        response_data["data"] = item
+        response = jsonify(response_data)
+    return response
+
+
+@app.errorhandler(404)
+def not_found(error):
+    response_data = {
+        "success": False,
+        "data": [],
+        "error": "Not found"
+    }
+    response = jsonify(response_data)
+    response.status_code = 404
     return response
 
 
